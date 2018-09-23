f037a2edd5ea68abd9ef1d37dd70b08571ffb20c
Tom Philip <tom@tomphilip.me>
2018-09-23 11:18:17 +0200

    Added change log

--- devicetypes/tommysqueak/qubino-flush-relay-thermostat.src/qubino-flush-relay-thermostat.groovy
+++ devicetypes/tommysqueak/qubino-flush-relay-thermostat.src/qubino-flush-relay-thermostat.groovy
@@ -3,7 +3,7 @@
  *
  *  https://github.com/tommysqueak/qubino-flush-relay-thermostat
  *
- *  Copyright 2017 Tom Philip
+ *  Copyright 2018 Tom Philip
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -13,6 +13,10 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Revision History
+ *  ==============================================
+ *  2018-09-23 - Added when the costs/kwH were reset and stored the previous costs.
  *
  */
 metadata {
